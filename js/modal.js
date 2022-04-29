f8da73817243a0d664ac29f6fcce3f40d16331a6
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-29 20:47:50 +0300

    close modal on EscKeyPress and onBackdropClick

--- js/modal.js
+++ js/modal.js
@@ -3,13 +3,30 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    window.addEventListener('keydown', onEscKeyPress);
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
+  }
+
+  refs.backdrop.addEventListener('click', onBackdropClick);
+
+  function onBackdropClick(e) {
+    if (e.currentTarget === e.target) {
+      toggleModal();
+    }
+  }
+
+  function onEscKeyPress(e) {
+    if (e.code === 'Escape') {
+      toggleModal();
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
   }
 })();
